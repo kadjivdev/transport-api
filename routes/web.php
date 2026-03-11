7b6f8e39529880f9ec5066d19769b2bc183d5b18
kadjivdev <kadjivdev@gmail.com>
2026-03-11 16:44:41 +0100

    revu du web.php

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ Route::get("/debug", function () {
     Log::debug("Les users : ", ["data" => User::all()]);
     return User::all();
 });
+
+// dernière route : toutes les requêtes non‑API reviennent au même point
+Route::get('/{any}', function () {
+    return view('app'); // ou welcome, app.blade.php, etc.
+})->where('any', '.*');
